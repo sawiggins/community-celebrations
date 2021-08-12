094fc6935c85b832ffbd9bdfec87ec449ea6cee6
Stephanie Wiggins <stephawiggins@gmail.com>
2021-08-12 11:02:46 -0500

    creates and handles form submissions for new cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -6,4 +6,23 @@ class CardsController < ApplicationController
   def show
     @card = Card.find(params[:id])
   end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(card_params)
+
+    if @card.save
+      redirect_to @card
+    else
+      render :new
+    end
+  end
+
+  private
+  def card_params
+    params.require(:card).permit(:title, :description)
+  end
 end
