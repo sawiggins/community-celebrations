bd615b994151668f6f668b7def69207c5718f8e4
Stephanie Wiggins <stephawiggins@gmail.com>
2021-08-13 07:38:48 -0500

    adds delete card functionality

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -35,6 +35,13 @@ class CardsController < ApplicationController
     end
   end
 
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+
+    redirect_to root_path
+  end
+
   private
   def card_params
     params.require(:card).permit(:title, :description)
